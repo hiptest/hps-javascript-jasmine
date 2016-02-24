331a03b5f987ae3af2056aa458606a19688a7305
Vincent Pretre <vincent.pretre@gmail.com>
2016-02-24 16:04:57 +0100

    Updated README and tests

--- README.md
+++ README.md
@@ -1,2 +1,38 @@
 # hps-javascript-jasmine
+[![Build Status](https://travis-ci.org/hiptest/hps-javascript-jasmine.svg?branch=master)](https://travis-ci.org/hiptest/hps-javascript-jasmine)
+
 Hiptest publisher samples with Javascript/Jasmine
+
+In this repository you'll find tests generated in Javascript/Jasmine format from [Hiptest](https://hiptest.net), using [Hiptest publisher](https://github.com/hiptest/hiptest-publisher).
+
+The goals are:
+
+ * to show how tests are exported in Javascript/Jasmine.
+ * to check exports work out of the box (well, with implemented actionwords)
+
+System under test
+------------------
+
+The SUT is a (not that much) simple coffee machine. You start it, you ask for a coffee and you get it, sometimes. But most of times you have to add water or beans, empty the grounds. You have an automatic expresso machine at work or at home? So you know how it goes :-)
+
+Update tests
+-------------
+
+
+To update the tests:
+
+    hiptest-publisher -c javascript-jasmine.conf --only=tests
+
+The tests are generated in [``tests/project_test.js``](https://github.com/hiptest/hps-javascript-jasmine/blob/master/tests/project_test.js)
+
+Run tests
+---------
+
+
+To build the project and run the tests, use the following command:
+
+    grunt jasmine
+
+The SUT implementation can be seen in [``src/coffee_machine.js``](https://github.com/hiptest/hps-javascript-jasmine/blob/master/src/coffee_machine.js)
+
+The test report is generated in ```_build/test-reports/jasmine/TEST-CoffeemachineHiptestpublishersample.xml```
--- tests/project_test.js
+++ tests/project_test.js
@@ -4,7 +4,8 @@ describe('Coffee machine - Hiptest publisher sample', function () {
     this.actionwords.sut = CoffeeMachine();
   });
 
-  it('Simple use (uid:f904d2e6-6882-45ab-9756-78af7a5dc7e4)', function () {
+  it('Simple use', function () {
+    // Well, sometimes, you just get a coffee.
     // Given the coffee machine is started
     this.actionwords.theCoffeeMachineIsStarted();
     // When I take a coffee
@@ -13,7 +14,54 @@ describe('Coffee machine - Hiptest publisher sample', function () {
     this.actionwords.coffeeShouldBeServed();
   });
 
-  it('Full grounds does not block coffee (uid:0ae88e38-c64c-4116-8a3e-66b11dc5f475)', function () {
+  it('Water runs away', function () {
+    // Simple scenario showing that after 50 coffees, the "Fill tank" message is displayed but it is still possible to have coffee until the tank is fully empty.
+    // Given the coffee machine is started
+    this.actionwords.theCoffeeMachineIsStarted();
+    // When fifty coffees have been taken without filling the tank
+    this.actionwords.fiftyCoffeesHaveBeenTakenWithoutFillingTheTank();
+    // Then message "Fill tank" should be displayed
+    this.actionwords.messageMessageShouldBeDisplayed("Fill tank");
+    // When I take a coffee
+    this.actionwords.iTakeACoffee();
+    // Then coffee should be served
+    this.actionwords.coffeeShouldBeServed();
+    // When I take "10" coffees
+    this.actionwords.iTakeCoffeeNumberCoffees(10);
+    // Then coffee should not be served
+    this.actionwords.coffeeShouldNotBeServed();
+    // And message "Fill tank" should be displayed
+    this.actionwords.messageMessageShouldBeDisplayed("Fill tank");
+    // When I fill the water tank
+    this.actionwords.iFillTheWaterTank();
+    // Then message "Ready" should be displayed
+    this.actionwords.messageMessageShouldBeDisplayed("Ready");
+  });
+
+  it('Beans run out', function () {
+    // Simple scenario showing that after 38 coffees, the message "Fill beans" is displayed but it is possible to take two coffees until there is no more beans.
+    // Given the coffee machine is started
+    this.actionwords.theCoffeeMachineIsStarted();
+    // When thirty eight coffees are taken without filling beans
+    this.actionwords.thirtyEightCoffeesAreTakenWithoutFillingBeans();
+    // Then coffee should be served
+    this.actionwords.coffeeShouldBeServed();
+    // And message "Fill beans" should be displayed
+    this.actionwords.messageMessageShouldBeDisplayed("Fill beans");
+    // When I take "2" coffees
+    this.actionwords.iTakeCoffeeNumberCoffees(2);
+    // Then coffee should be served
+    this.actionwords.coffeeShouldBeServed();
+    // And message "Fill beans" should be displayed
+    this.actionwords.messageMessageShouldBeDisplayed("Fill beans");
+    // When I take a coffee
+    this.actionwords.iTakeACoffee();
+    // Then coffee should not be served
+    this.actionwords.coffeeShouldNotBeServed();
+  });
+
+  it('Full grounds does not block coffee', function () {
+    // You keep getting coffee even if the "Empty grounds" message is displayed. That said it's not a fantastic idea, you'll get ground everywhere when you'll decide to empty it.
     // Given the coffee machine is started
     this.actionwords.theCoffeeMachineIsStarted();
     // When I take "29" coffees
@@ -38,73 +86,31 @@ describe('Coffee machine - Hiptest publisher sample', function () {
     this.actionwords.messageMessageShouldBeDisplayed("Empty grounds");
   });
 
-  it('Water runs away (uid:83bdb7a4-e845-484d-961a-c521eca436f4)', function () {
-    // Given the coffee machine is started
-    this.actionwords.theCoffeeMachineIsStarted();
-    // When fifty coffees have been taken without filling the tank
-    this.actionwords.fiftyCoffeesHaveBeenTakenWithoutFillingTheTank();
-    // Then message "Fill tank" should be displayed
-    this.actionwords.messageMessageShouldBeDisplayed("Fill tank");
-    // When I take a coffee
-    this.actionwords.iTakeACoffee();
-    // Then coffee should be served
-    this.actionwords.coffeeShouldBeServed();
-    // When I take "10" coffees
-    this.actionwords.iTakeCoffeeNumberCoffees(10);
-    // Then coffee should not be served
-    this.actionwords.coffeeShouldNotBeServed();
-    // And message "Fill tank" should be displayed
-    this.actionwords.messageMessageShouldBeDisplayed("Fill tank");
-    // When I fill the water tank
-    this.actionwords.iFillTheWaterTank();
-    // Then message "Ready" should be displayed
-    this.actionwords.messageMessageShouldBeDisplayed("Ready");
-  });
-
-  it('Beans run out (uid:b4b81e5f-931c-436e-a84a-fabf841c95aa)', function () {
-    // Given the coffee machine is started
-    this.actionwords.theCoffeeMachineIsStarted();
-    // When thirty eight coffees are taken without filling beans
-    this.actionwords.thirtyEightCoffeesAreTakenWithoutFillingBeans();
-    // Then coffee should be served
-    this.actionwords.coffeeShouldBeServed();
-    // And message "Fill beans" should be displayed
-    this.actionwords.messageMessageShouldBeDisplayed("Fill beans");
-    // When I take "2" coffees
-    this.actionwords.iTakeCoffeeNumberCoffees(2);
-    // Then coffee should be served
-    this.actionwords.coffeeShouldBeServed();
-    // And message "Fill beans" should be displayed
-    this.actionwords.messageMessageShouldBeDisplayed("Fill beans");
-    // When I take a coffee
-    this.actionwords.iTakeACoffee();
-    // Then coffee should not be served
-    this.actionwords.coffeeShouldNotBeServed();
-  });
-
-  it('No messages are displayed when machine is shut down (uid:bc7c5f52-4cfa-49c8-a596-1b045d60565d)', function () {
-    // Given the coffee machine is started
-    this.actionwords.theCoffeeMachineIsStarted();
-    // When I shutdown the coffee machine
-    this.actionwords.iShutdownTheCoffeeMachine();
-    // Then message "" should be displayed
-    this.actionwords.messageMessageShouldBeDisplayed();
-  });
-
   describe('Messages are based on language', function () {
     function messagesAreBasedOnLanguage (lang, ready_message) {
+      // Well, sometimes, you just get a coffee.
       // When I start the coffee machine "<lang>"
       this.actionwords.iStartTheCoffeeMachine(lang);
       // Then message "<ready_message>" should be displayed
       this.actionwords.messageMessageShouldBeDisplayed(ready_message);
     }
 
-    it('English (uid:95bf5746-82c0-4f3f-9fba-01616cd9a6e0)', function () {
+    it('English', function () {
       messagesAreBasedOnLanguage.apply(this, ['en', 'Ready']);
     });
 
-    it('French (uid:a2be1026-0a37-47b8-98b9-f223f40ef469)', function () {
+    it('French', function () {
       messagesAreBasedOnLanguage.apply(this, ['fr', 'Pret']);
     });
+  });
+
+
+  it('No messages are displayed when machine is shut down', function () {
+    // Given the coffee machine is started
+    this.actionwords.theCoffeeMachineIsStarted();
+    // When I shutdown the coffee machine
+    this.actionwords.iShutdownTheCoffeeMachine();
+    // Then message "" should be displayed
+    this.actionwords.messageMessageShouldBeDisplayed();
   });
 });
