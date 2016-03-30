82d4ce2c8ba654e605abdf66965664f44ff6f33c
Vincent Pretre <vincent.pretre@gmail.com>
2016-03-30 16:09:26 +0200

    Update gruntfile to use new test files

--- Gruntfile.js
+++ Gruntfile.js
@@ -5,7 +5,13 @@ module.exports = function(grunt) {
     jasmine: {
       src: ['src/coffee_machine.js', 'tests/actionwords.js'],
       options: {
-        specs: ['tests/project_test.js'],
+        specs: [
+          'tests/nominal_case_test.js',
+          'tests/weird_specs_test.js',
+          'tests/error_messages/beans_test.js',
+          'tests/error_messages/grounds_test.js',
+          'tests/error_messages/water_test.js'
+        ],
         junit: {
           path: '_build/test-reports/jasmine',
           consolidate: true
